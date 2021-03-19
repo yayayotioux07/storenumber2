2c6feb8d77a58225f936d693e9fe947b4d54bbbd
yayotioux <>
2021-03-19 05:28:59 -0500

    updated app with images in preview mode

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Route } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
+
 
 function App() {
   return (
     <div>
-    <HomePage/>
+      <Route exact path= '/' component = {HomePage} />
+      <Route path= '/shop' component = {ShopPage} />
+
     </div>
   );
 }
