8e96e1131376a0984fbc1faf8369c845695fd71c
yayotioux <>
2021-03-23 20:23:05 -0500

    added signin/out button w/authentication

--- src/App.js
+++ src/App.js
@@ -7,18 +7,46 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component.jsx';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
+import { auth } from './firebase/firebase.utils';
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  unsubcribeFromAuth = null
+
+    componentDidMount() {
+      auth.onAuthStateChanged(user => {
+        this.setState({currentUser: user});
+
+        console.log(user);
+      });
+  }
+
+  componentWillUnmount() {
+    this.unsubcribeFromAuth();
+  }
 
 
-function App() {
-  return (
-    <div>
-    <Header/>
-    <Switch>
-      <Route exact path= '/' component = {HomePage} />
-      <Route path= '/shop' component = {ShopPage} />
-    </Switch>
-    </div>
-  );
+  render() {
+    return (
+      <div>
+      <Header currentUser={this.state.currentUser} />
+      <Switch>
+        <Route exact path= '/' component = {HomePage} />
+        <Route path= '/shop' component = {ShopPage} />
+        <Route path= '/signin' component = {SignInAndSignUpPage} />
+  
+      </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config =  {
+    apiKey: "AIzaSyDuJSbgp9Uqk6xb5CXReQF42_b5ngwyZow",
+    authDomain: "crwn-clothing-db-7a59f.firebaseapp.com",
+    projectId: "crwn-clothing-db-7a59f",
+    storageBucket: "crwn-clothing-db-7a59f.appspot.com",
+    messagingSenderId: "327910579580",
+    appId: "1:327910579580:web:1ef51db6c16627ef8318f9",
+    measurementId: "G-RSC3Z1PXDV"
+  };
+
+  firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({prompt: 'select_account'});
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
+
+
