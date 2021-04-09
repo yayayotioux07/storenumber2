5048547c95dcff4fe7c497ec55f95a4ba30b5aba
yayotioux <>
2021-04-09 07:11:15 -0500

    newest 116

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component.jsx';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -19,15 +19,26 @@ class App extends React.Component {
     };
   }
 
-  unsubcribeFromAuth = null
+  unsubcribeFromAuth = null;
 
     componentDidMount() {
-      auth.onAuthStateChanged(user => {
-        this.setState({currentUser: user});
+      this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+        if (userAuth) {
+          const userRef = await createUserProfileDocument(userAuth);
 
-        console.log(user);
+          userRef.onSnapshot(snapShot => {
+            this.setState({
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data()
+              }
+            });
+          });
+        }
+        this.setState({currentUser: userAuth});
       });
-  }
+    }
+  
 
   componentWillUnmount() {
     this.unsubcribeFromAuth();
